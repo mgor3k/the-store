df2f0b5bc2ad23bcf0e97ac0cb92e0d5e2ec828c
Maciej Górecki <maciekgorecki@icloud.com>
2023-07-29 10:01:01 +0200

    Initial Commit

---
+++ TheStore/TheStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStoreApp.swift
+//  TheStore
+//
+//  Created by Maciej Gorecki on 29/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
