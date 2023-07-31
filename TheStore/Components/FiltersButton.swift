d7b9d6c133389aae0f34eef78ea927312c695df9
Maciej Górecki <maciekgorecki@icloud.com>
2023-07-31 22:11:16 +0200

    Updated to proper filter button.

--- TheStore/Components/FiltersButton.swift
+++ TheStore/Components/FiltersButton.swift
@@ -4,21 +4,47 @@ import SwiftUI
 
 struct FiltersButton: View {
   var body: some View {
-    Image(systemName: "line.3.horizontal")
-      .padding()
-      .frame(maxHeight: .infinity)
-      .background(
-        Color.gray.opacity(0.1)
-          .clipShape(
-            RoundedRectangle(
-              cornerRadius: 18,
-              style: .continuous
-            )
+    VStack {
+      HStack(spacing: 2) {
+        Rectangle()
+          .foregroundStyle(.orange)
+          .frame(width: 4)
+          .clipShape(Capsule())
+        Rectangle()
+          .foregroundStyle(.orange)
+          .frame(width: 10)
+          .clipShape(Capsule())
+      }
+      .frame(height: 3)
+
+      HStack(spacing: 2) {
+        Rectangle()
+          .foregroundStyle(.gray)
+          .frame(width: 10)
+          .clipShape(Capsule())
+
+        Rectangle()
+          .foregroundStyle(.gray)
+          .frame(width: 4)
+          .clipShape(Capsule())
+      }
+      .frame(height: 3)
+    }
+    .padding()
+    .frame(maxHeight: .infinity)
+    .background(
+      Color.gray.opacity(0.1)
+        .clipShape(
+          RoundedRectangle(
+            cornerRadius: 18,
+            style: .continuous
           )
-      )
+        )
+    )
   }
 }
 
 #Preview {
   FiltersButton()
+    .frame(height: 44)
 }
