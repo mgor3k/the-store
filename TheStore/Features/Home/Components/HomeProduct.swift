82cb1594abed36b5682805ea8dabd1b5e48e4319
Maciej Górecki <maciekgorecki@icloud.com>
2023-08-04 22:53:35 +0200

    UI fixes.

--- TheStore/Features/Home/Components/HomeProduct.swift
+++ TheStore/Features/Home/Components/HomeProduct.swift
@@ -6,64 +6,70 @@ struct HomeProduct: View {
   let product: Product
 
   var body: some View {
-    HStack(spacing: 8) {
-      GeometryReader { proxy in
-        Color.color1Accent
-          .clipShape(
-            RoundedRectangle(
-              cornerRadius: 12,
-              style: .continuous
+    GeometryReader { proxy in
+      HStack(spacing: 8) {
+        GeometryReader { proxy in
+          Color.color1Accent
+            .clipShape(
+              RoundedRectangle(
+                cornerRadius: 12,
+                style: .continuous
+              )
             )
-          )
-          .rotationEffect(.degrees(-15))
-          .frame(
-            width: proxy.size.width / 2.5,
-            height: 90
-          )
-          .padding()
-          .frame(
-            width: proxy.size.width,
-            height: proxy.size.height,
-            alignment: .center
-          )
-      }
+            .rotationEffect(.degrees(-15))
+            .frame(
+              width: proxy.size.width * 0.5,
+              height: 100
+            )
+            .offset(x: 16)
+            .padding()
+            .frame(
+              width: proxy.size.width,
+              height: proxy.size.height,
+              alignment: .center
+            )
+        }
+        .frame(width: proxy.size.width * 0.4)
 
-      VStack(alignment: .leading, spacing: 16) {
-        VStack(alignment: .leading) {
-          Text(product.name)
-            .font(.title)
+        VStack(alignment: .leading, spacing: 16) {
+          VStack(alignment: .leading) {
+            Text(product.name)
+              .font(.title3)
 
-          Text("by nike")
+            Text("by nike")
+              .font(.callout)
+              .foregroundStyle(.secondary)
+          }
+
+          Spacer()
+
+          Text("Your workhorse with wing returns.")
             .font(.callout)
             .foregroundStyle(.secondary)
         }
-
-        Text("Your workhorse with wing returns.")
-          .font(.callout)
-          .foregroundStyle(.secondary)
+        .padding(24)
+        .frame(maxWidth: .infinity, alignment: .leading)
       }
-
-      Spacer()
+      .background(Color.color1)
+      .clipShape(
+        RoundedRectangle(
+          cornerRadius: 28,
+          style: .continuous
+        )
+      )
+      .overlay(alignment: .topLeading) {
+        Button(action: {}) {
+          Image(systemName: product.isLiked ? "heart.fill" : "heart")
+            .font(.caption)
+            .padding(6)
+            .background(Color.white)
+            .clipShape(Circle())
+        }
+        .padding(.vertical, 14)
+        .padding(.horizontal, 12)
+      }
     }
     .frame(height: 180)
-    .background(Color.color1)
-    .clipShape(
-      RoundedRectangle(
-        cornerRadius: 28,
-        style: .continuous
-      )
-    )
-    .overlay(alignment: .topLeading) {
-      Button(action: {}) {
-        Image(systemName: product.isLiked ? "heart.fill" : "heart")
-          .font(.caption)
-          .padding(6)
-          .background(Color.white)
-          .clipShape(Circle())
-      }
-      .padding(.vertical, 14)
-      .padding(.horizontal, 12)
-    }
   }
 }
 
