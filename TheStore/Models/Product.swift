6786cc9d9d62dfabc97a8e3d6b95df9f012889f3
Maciej Górecki <maciekgorecki@icloud.com>
2023-08-06 11:56:43 +0200

    Added more mocks.

--- TheStore/Models/Product.swift
+++ TheStore/Models/Product.swift
@@ -15,6 +15,7 @@ extension Product {
   static let mock: [Self] = [
     .init(id: "1", name: "Nike Air Pegasus", hexColor: "#FFD9D8", isLiked: true),
     .init(id: "2", name: "Nike ZoomX", hexColor: "#DBE3E6", isLiked: false),
-    .init(id: "3", name: "Nike Air Jordan", hexColor: "#FFD9D8", isLiked: false)
+    .init(id: "3", name: "Nike Air Jordan", hexColor: "#FFD9D8", isLiked: false),
+    .init(id: "4", name: "Nike Vapormax", hexColor: "#DBE3E6", isLiked: false)
   ]
 }
